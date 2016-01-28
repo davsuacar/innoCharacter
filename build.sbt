97e738230ffabe42af29424e71f23305a1c46e29
David Suarez <david.suarez@intelygenz.com>
2016-01-28 12:54:30 +0100

    Twitter Streaming

--- build.sbt
+++ build.sbt
@@ -6,4 +6,5 @@ scalaVersion := "2.10.4"
 
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.10" % "1.6.0"
 libraryDependencies += "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.6.0"
+libraryDependencies += "org.twitter4j" % "twitter4j-stream" % "3.0.3"
     
--- src/main/scala/AuthTwitter.scala
+++ src/main/scala/AuthTwitter.scala
@@ -3,6 +3,9 @@ package AuthTwitter
 import org.apache.spark._
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.twitter._
+import twitter4j.TwitterFactory
+import twitter4j.auth.{OAuth2Authorization, AuthorizationFactory, Authorization}
 
 
 /**
@@ -20,11 +23,25 @@ object AuthTwitter {
     val conf = new SparkConf().setMaster("local[2]").setAppName("innoCharacter")
     val ssc = new StreamingContext(conf, Seconds(3))
 
-    // Create a DStream that will connect to hostname:port, like localhost:9999
-    val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_AND_DISK_SER)
+    val twitterAuth = new twitter4j.conf.ConfigurationBuilder()
+      .setOAuthConsumerKey("")
+      .setOAuthConsumerSecret("")
+      .setOAuthAccessToken("")
+      .setOAuthAccessTokenSecret("")
+      .build
+
+    val twitter_auth = new TwitterFactory(twitterAuth)
+    val a = new twitter4j.auth.OAuthAuthorization(twitterAuth)
+    val atwitter : Option[twitter4j.auth.Authorization] =  Some(twitter_auth.getInstance(a).getAuthorization())
+
+    val filters = Seq("@DavidIntelygenz")
+    val twitterStream = TwitterUtils.createStream(ssc, atwitter, filters, StorageLevel.MEMORY_AND_DISK)
 
     // Split each line into words
-    val words = lines.flatMap(_.split(" "))
+    val words = twitterStream.flatMap(status => {
+      println("TWITTER---->" + status.getText)
+      status.getText.split(" ")
+    })
 
     val pairs = words.map(word => (word, 1))
     val wordCounts = pairs.reduceByKey(_ + _)
